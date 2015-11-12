5328e6b26428e975d4a1541afb961489bca966d3
Riwei Chen <riwei.chen@outlook.com>
2015-11-12 21:59:12 +0800

    first commit

---
+++ FaceAlignment/Evaluate/evaluate.py
@@ -0,0 +1,84 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov  7 20:39:22 2014
+
+@author: crw
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+import cv2
+
+caffe_root = '/home/crw/caffe-local/'
+
+import sys
+sys.path.insert(0, caffe_root + 'python')
+
+import caffe
+#指定一些图形显示的参数;
+plt.rcParams['figure.figsize'] = (10, 10)
+plt.rcParams['image.interpolation'] = 'nearest'
+plt.rcParams['image.cmap'] = 'gray'
+
+itera=8000000
+net = caffe.Classifier('../try1_2/depoly.prototxt',
+      '/media/crw/MyBook/Model/FaceAlignment/try1_2/snapshot_iter_' +str(itera)+'.caffemodel')
+
+#指定网络的一个格式;
+net.set_phase_test()
+net.set_mode_cpu()
+
+def vis_square(data, padsize=1, padval=0):
+    '''
+    @brief: 定义显示函数;
+    '''
+    # 归一化到0-1
+    data -= data.min()
+    data /= data.max()
+    
+    # 强制使得滤波器为方形;
+    n = int(np.ceil(np.sqrt(data.shape[0])))
+    padding = ((0, n ** 2 - data.shape[0]), (0, padsize), (0, padsize)) + ((0, 0),) * (data.ndim - 3)
+    data = np.pad(data, padding, mode='constant', constant_values=(padval, padval))
+    
+    # 把滤波器序列转换成图像显示;
+    data = data.reshape((n, n) + data.shape[1:]).transpose((0, 2, 1, 3) + tuple(range(4, data.ndim + 1)))
+    data = data.reshape((n * data.shape[1], n * data.shape[3]) + data.shape[4:])
+    #显示数据：
+    plt.imshow(data)
+
+    
+def test_one(image_path):
+    '''
+    @brief: 测试模型，输入为图像路径
+    '''
+    #image=skimage.io.imread(image_path)
+    image=caffe.io.load_image(image_path) 
+    print image.shape
+    #image=cv2.imread(image_path) 
+    print image.shape
+    
+    scores = net.predict([image], False)
+    print scores.shape
+    scores = np.reshape(scores,(10,))
+    print scores.shape
+    print scores
+    image=cv2.imread(image_path) 
+    for  i in range(0,10,2):
+        cv2.circle(image,(int(scores[i]),int(scores[i+1])),1,[0,0,255])
+    #image = cv2.resize(image,(200,200))
+    #cv2.imshow('Result',image)
+    #cv2.waitKey(200)
+    return image
+
+if __name__  ==  "__main__":
+    savepath= '/media/crw/MyBook/Experiment/FaceAlignment/Evaluate/result/';
+    for i in range(1,200):
+        
+        image_path ='/media/crw/MyBook/MyDataset/FacePoint/test39X39/'+str(i)+'.jpg'
+        
+        image = test_one(image_path)
+        print image.shape
+        cv2.imwrite(savepath + str(i)+'.jpg',image)
+        
---
+++ FaceAlignment/Evaluate/recovery_all.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov  7 20:39:22 2014
+
+@author: crw
+"""
+
+#先导入一些模块;
+import PIL.Image as Image
+import numpy as np
+import caffe
+import os.path
+import sys
+
+caffe_root = '/home/crw/caffe-local/'
+
+sys.path.insert(0, caffe_root + 'python')
+
+net = caffe.Classifier(caffe_root+'faceexp/try1_2/face_exp.prototxt',
+                                   '/media/crw/DataCenter/ResultModel/faceexp/try1_2/snapshot_iter_30000.caffemodel')
+net.set_phase_test()
+net.set_mode_cpu()
+
+j=1
+BASE_PATH='/media/crw/DataCenter/TestImage/FRGC100'
+for dirname, dirnames, filenames in os.walk(BASE_PATH):
+    for subdirname in dirnames: # 每一类。
+        subject_path = os.path.join(dirname, subdirname)
+        os.mkdir('/media/crw/DataCenter/ResultImage/faceexp/try1_2/FRGC/'+subdirname)
+        j=1
+        for filename in os.listdir(subject_path):#每一个图片
+            abs_path = "%s/%s" % (subject_path, filename)
+            image=caffe.io.load_image(abs_path)
+            image=image*255
+            scores = net.predict([image], False)
+            newscores= (scores[:,:]-scores.min())/scores.max()*255
+            reconstructpic=np.reshape(newscores,(64,64))
+            img=Image.fromarray(reconstructpic)
+            img = img.convert('RGB')
+            
+            savename='/media/crw/DataCenter/ResultImage/faceexp/try1_2/FRGC/'+subdirname+'/%d.jpg' %( j )
+            j=j+1;
+            img.save(savename)
+            print abs_path
+                
+print 'done!'
